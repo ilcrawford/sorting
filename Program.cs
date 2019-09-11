2dc2cf3cb04d0cefe866fccbe31174bda5cb3903
Iman Crawford <ilcrawford@gmail.com>
2019-09-10 22:30:18 -0500

    New random list

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace sorting
     {
         static void Main(string[] args)
         {
-            int[] list = new[]{3,5,1,10,7,6,2,8,9};
+            int[] list = new[]{ 38, 92, 28, 3, 71, 50, 14, 24, 20, 66, 70, 45, 17, 9, 99, 38 };
             Console.WriteLine("Hello World!");
             ISort sort = new bubbleSort();
             int[] newList = sort.Sort(list);
